d25a9571c6d917d95ee51f35e1e0dcc8792bd4d2
sfotedar995 <161820636+sfotedar995@users.noreply.github.com>
2024-06-17 18:07:50 +0530

    Update StringCalculator.java

--- StringCalculator.java
+++ StringCalculator.java
@@ -17,7 +17,7 @@ public class StringCalculator {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.out.println(add("//;\n1;2"));
+        System.out.println(add("1,2"));
 
     }
 }
