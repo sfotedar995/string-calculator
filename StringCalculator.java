1817e8e7244e7e40d28a7396c722e29690b2b528
sfotedar995 <161820636+sfotedar995@users.noreply.github.com>
2024-06-17 20:12:39 +0530

    Added the changes for handling commas

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,10 +1,10 @@
 public class StringCalculator {
-    public static int add(String numbers) {
+public static int add(String numbers) {
         if (numbers == null || numbers.isEmpty()) {
             return 0;
         }
 
-        String[] nums = numbers.split(",");
+        String[] nums = numbers.split(",|\n");
         int sum = 0;
 
         for (String num : nums) {
@@ -15,8 +15,9 @@ public class StringCalculator {
     }
 
     public static void main(String[] args) {
-        System.out.println(add("1,2"));
+        System.out.println(add("1,2,3"));
         System.out.println(add("1,2,3,4,5,6,7,8,9,10"));
+        System.out.println(add("1\n2,3"));
 
     }
 }
