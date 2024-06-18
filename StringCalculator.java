66aba6e4182b789e8538932e8e46b84bf3915466
sfotedar995 <161820636+sfotedar995@users.noreply.github.com>
2024-06-18 20:16:21 +0530

    Added the changes for newline character constraint

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,10 +1,17 @@
 public class StringCalculator {
 public static int add(String numbers) {
-        if (numbers == null || numbers.isEmpty()) {
+     if (numbers == null || numbers.isEmpty()) {
             return 0;
         }
 
-        String[] nums = numbers.split(",|\n");
+        String delimiter = ",|\n";
+        if (numbers.startsWith("//")) {
+            int delimiterEnd = numbers.indexOf("\n");
+            delimiter = numbers.substring(2, delimiterEnd);
+            numbers = numbers.substring(delimiterEnd + 1);
+        }
+
+        String[] nums = numbers.split(delimiter);
         int sum = 0;
 
         for (String num : nums) {
@@ -12,7 +19,7 @@ public static int add(String numbers) {
         }
 
         return sum;
-    }
+}
 
     public static void main(String[] args) {
         System.out.println(add("1,2,3"));
